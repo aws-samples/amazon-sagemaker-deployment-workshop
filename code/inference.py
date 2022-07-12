d64799b08cc5c31d1aa1dc8200ad098d0a2a2e0b
Mel Li <lpy1222@gmail.com>
2022-07-12 04:08:52 +0000

    update lab1 notebook and inference.py

--- code/inference.py
+++ code/inference.py
@@ -1,7 +1,8 @@
 from transformers import pipeline
+import json
 
 CSV_CONTENT_TYPE = 'text/csv'
-
+JSON_CONTENT_TYPE = 'application/json'
 
 def model_fn(model_dir):
     sentiment_analysis = pipeline(
@@ -16,6 +17,10 @@ def model_fn(model_dir):
 def input_fn(serialized_input_data, content_type=CSV_CONTENT_TYPE):
     if content_type == CSV_CONTENT_TYPE:
         input_data = serialized_input_data.splitlines()
+        return input_data
+    elif content_type == JSON_CONTENT_TYPE:
+        data = json.loads(serialized_input_data)
+        input_data = data.pop("inputs", data)
         return input_data
     else:
         raise Exception('Requested unsupported ContentType in Accept: ' + content_type)
