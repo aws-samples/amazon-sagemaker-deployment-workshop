6a739f646b8a73a2d774b5c0214785e9c1d019bd
Mel Li <lpy1222@gmail.com>
2022-07-16 10:42:48 +0000

    update blue green deployment notebook and images

--- utils.py
+++ utils.py
@@ -98,7 +98,7 @@ def get_invocation_metrics_for_endpoint_variant(endpoint_name, variant_name, sta
 
 
 def plot_endpoint_metrics(endpoint_name=None, variant1=None, variant2=None, start_time=None):
-    start_time = start_time or datetime.now() - timedelta(minutes=60)
+    start_time = start_time or datetime.now() - datetime.timedelta(minutes=60)
     end_time = datetime.datetime.now()
     metrics_variant1 = get_invocation_metrics_for_endpoint_variant(
         endpoint_name, variant1["VariantName"], start_time, end_time
@@ -153,3 +153,114 @@ def endpoint_update_wait(endpoint_name: str):
         time.sleep(5)
 
     return 
+
+def invoke_endpoint_max_invocations(
+    endpoint_name, max_invocations=100, wait_interval_sec=1, should_raise_exp=False
+):
+    print(f"Sending test traffic to the endpoint {endpoint_name}. \nPlease wait...")
+
+    nm_rounds = round(max_invocations/100)
+    count = 0
+    for i in range(nm_rounds):
+        with open("../sample_payload/batch_data.csv", "r") as f:
+            for row in f:
+                payload = row.rstrip("\n")
+                try:
+                    response = sm_runtime.invoke_endpoint(
+                        EndpointName=endpoint_name, ContentType="text/csv", Body=payload
+                    )
+                    response["Body"].read()
+                    print(".", end="", flush=True)
+                except Exception as e:
+                    print("E", end="", flush=True)
+                    if should_raise_exp:
+                        raise e
+                count += 1
+                if count > max_invocations:
+                    break
+                time.sleep(wait_interval_sec)
+
+    print("\nDone!")
+    return
+
+
+def get_sagemaker_metrics(
+    endpoint_name,
+    endpoint_config_name,
+    variant_name,
+    metric_name,
+    statistic,
+    start_time,
+    end_time,
+):
+    dimensions = [
+        {"Name": "EndpointName", "Value": endpoint_name},
+        {"Name": "VariantName", "Value": variant_name},
+    ]
+    if endpoint_config_name is not None:
+        dimensions.append({"Name": "EndpointConfigName", "Value": endpoint_config_name})
+    metrics = cw.get_metric_statistics(
+        Namespace="AWS/SageMaker",
+        MetricName=metric_name,
+        StartTime=start_time,
+        EndTime=end_time,
+        Period=60,
+        Statistics=[statistic],
+        Dimensions=dimensions,
+    )
+    rename = endpoint_config_name if endpoint_config_name is not None else "ALL"
+    if len(metrics["Datapoints"]) == 0:
+        return
+    return (
+        pd.DataFrame(metrics["Datapoints"])
+        .sort_values("Timestamp")
+        .set_index("Timestamp")
+        .drop(["Unit"], axis=1)
+        .rename(columns={statistic: rename})
+    )
+
+
+def plot_endpoint_invocation_metrics(
+    endpoint_name,
+    endpoint_config_name,
+    variant_name,
+    metric_name,
+    statistic,
+    start_time=None,
+):
+    start_time = start_time or datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(minutes=60)
+    end_time = datetime.datetime.now(datetime.timezone.utc)
+    metrics_variants = get_sagemaker_metrics(
+        endpoint_name,
+        endpoint_config_name,
+        variant_name,
+        metric_name,
+        statistic,
+        start_time,
+        end_time,
+    )
+    if metrics_variants is None:
+        return
+    metrics_variants.plot(title=f"{metric_name}-{statistic}")
+    return metrics_variants
+
+def create_auto_rollback_alarm(
+    alarm_name, endpoint_name, variant_name, metric_name, statistic, threshold
+):
+    cw.put_metric_alarm(
+        AlarmName=alarm_name,
+        AlarmDescription="Test SageMaker endpoint deployment auto-rollback alarm",
+        ActionsEnabled=False,
+        Namespace="AWS/SageMaker",
+        MetricName=metric_name,
+        Statistic=statistic,
+        Dimensions=[
+            {"Name": "EndpointName", "Value": endpoint_name},
+            {"Name": "VariantName", "Value": variant_name},
+        ],
+        Period=60,
+        EvaluationPeriods=1,
+        Threshold=threshold,
+        ComparisonOperator="GreaterThanOrEqualToThreshold",
+        TreatMissingData="notBreaching",
+    )
